b8c38d42f482209e1dfa22b64b5df59169994873
Stephen-Roach <stephenroach21@gmail.com>
2021-10-20 12:44:08 -0700

    creates the article shema and model

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const port = 3000;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/test');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true });
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model('Article', articleSchema);
 
 app.get('/', (req, res) => {
   res.send('Hello HUmans');
