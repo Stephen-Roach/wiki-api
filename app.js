53c8085e9428f177d57ca94bb7f4d252c48d1e92
Stephen-Roach <stephenroach21@gmail.com>
2021-10-19 22:22:59 -0700

    added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = 3000;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 app.get('/', (req, res) => {
   res.send('Hello HUmans');
